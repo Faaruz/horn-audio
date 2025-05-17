a312e8678eb779b9303973ecfa0875328fcb4fe7
Darkwarden <faaruz.uddin@gmail.com>
2025-05-17 21:28:02 +0200

    .

---
+++ src/main/java/com/hornsound/HornSoundConfig.java
@@ -0,0 +1,37 @@
+package com.hornsound;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
+
+@ConfigGroup("Horn Sound")
+public interface HornSoundConfig extends Config
+{
+	@ConfigItem(
+			keyName = "volume",
+			name = "Volume",
+			description = "Set the horn volume (0% to 100%)",
+			position = 1
+	)
+	@Range(
+			min = 0,
+			max = 100
+	)
+	default int volume()
+	{
+		return 100; // default to full volume
+	}
+
+	@ConfigItem(
+			keyName = "disableEncouragedByOthers",
+			name = "Disable other players horn",
+			description = "Don't play sound when others encourage you with the horn",
+			position = 2
+	)
+	default boolean disableEncouragedByOthers()
+	{
+		return false;
+	}
+
+}
---
+++ src/main/java/com/hornsound/HornSoundPlugin.java
@@ -0,0 +1,124 @@
+package com.hornsound;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.api.Client;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import java.util.Objects;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Horn Sound"
+)
+public class HornSoundPlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private HornSoundConfig config;
+
+	private Clip clip = null;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.info("Horn Sound started!");
+
+		try
+		{
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                    Objects.requireNonNull(getClass().getResource("/horn.wav"))); // make sure this is in src/main/resources
+			clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+		}
+		catch (Exception e)
+		{
+			log.warn("Failed to load horn sound", e);
+		}
+	}
+
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		if (clip != null && clip.isRunning())
+		{
+			clip.stop();
+		}
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		if (event.getType() != ChatMessageType.GAMEMESSAGE && event.getType() != ChatMessageType.SPAM)
+		{
+			return;
+		}
+
+		String message = event.getMessage();
+
+		if (message.contains("You encourage nearby allies"))
+		{
+			log.info("Horn activated (you encouraged others) - playing sound!");
+			playSound();
+		}
+		else if (message.contains("encourages you with") && !config.disableEncouragedByOthers())
+		{
+			log.info("Horn activated (you were encouraged) - playing sound!");
+			playSound();
+		}
+	}
+
+	private void playSound()
+	{
+		if (clip != null)
+		{
+			if (clip.isRunning())
+			{
+				clip.stop();
+			}
+			clip.setFramePosition(0);
+
+			try
+			{
+				FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				float volume = config.volume();
+
+				float minDb = -20.0f;
+				float maxDb = 6.0f;
+
+				float dB = (volume == 0)
+						? gainControl.getMinimum()
+						: minDb + (volume / 100.0f) * (maxDb - minDb);
+
+				gainControl.setValue(dB);
+			}
+			catch (IllegalArgumentException e)
+			{
+				log.warn("Volume control not supported on this system/clip", e);
+			}
+
+			clip.start();
+		}
+	}
+
+
+	@Provides
+	HornSoundConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(HornSoundConfig.class);
+	}
+}
