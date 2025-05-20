b76c67ee47984af53ab5e6fc780b5e3f01515a91
Darkwarden <faaruz.uddin@gmail.com>
2025-05-20 16:11:43 +0200

    Cleanup code and use RUNELITE_DIR instead of user.home.

--- src/main/java/com/hornsound/HornSoundPlugin.java
+++ src/main/java/com/hornsound/HornSoundPlugin.java
@@ -1,66 +1,62 @@
 package com.hornsound;
 
 import com.google.inject.Provides;
-import javax.inject.Inject;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
-import net.runelite.api.events.ChatMessage;
 import net.runelite.api.Client;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.client.RuneLite;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import javax.inject.Inject;
+import javax.sound.sampled.*;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.Objects;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Horn Sound"
+		name = "Horn Sound"
 )
 public class HornSoundPlugin extends Plugin
 {
+	private static final File PLUGIN_DIR = new File(RuneLite.RUNELITE_DIR, "horn-sound");
+	private static final String HORN_FILE_NAME = "horn.wav";
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private HornSoundConfig config;
 
-	private Clip clip = null;
+	private Clip clip;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Horn Sound started!");
-
 		try
 		{
-			File pluginDir = new File(System.getProperty("user.home"), ".runelite/horn-sound");
-			File customHorn = new File(pluginDir, "horn.wav");
-
-			if (!pluginDir.exists() && pluginDir.mkdirs())
+			if (!PLUGIN_DIR.exists() && !PLUGIN_DIR.mkdirs())
 			{
-				log.info("Created horn-sound plugin directory at {}", pluginDir.getAbsolutePath());
+				log.warn("Failed to create plugin directory: {}", PLUGIN_DIR.getAbsolutePath());
+				return;
 			}
 
-			// Optional: copy default horn if file doesn't exist
-			if (!customHorn.exists())
+			File hornFile = new File(PLUGIN_DIR, HORN_FILE_NAME);
+
+			if (!hornFile.exists())
 			{
-				copyDefaultHornToFolder(customHorn);
+				copyDefaultHornToFile(hornFile);
 			}
 
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(customHorn);
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(hornFile);
 			clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
-
-			log.info("Horn loaded from {}", customHorn.getAbsolutePath());
 		}
 		catch (Exception e)
 		{
@@ -68,13 +64,17 @@ public class HornSoundPlugin extends Plugin
 		}
 	}
 
-
 	@Override
 	protected void shutDown() throws Exception
 	{
-		if (clip != null && clip.isRunning())
+		if (clip != null)
 		{
-			clip.stop();
+			if (clip.isRunning())
+			{
+				clip.stop();
+			}
+			clip.close();
+			clip = null;
 		}
 	}
 
@@ -90,52 +90,53 @@ public class HornSoundPlugin extends Plugin
 
 		if (message.contains("You encourage nearby allies"))
 		{
-			log.info("Horn activated (you encouraged others) - playing sound!");
 			playSound();
 		}
 		else if (message.contains("encourages you with") && !config.disableEncouragedByOthers())
 		{
-			log.info("Horn activated (you were encouraged) - playing sound!");
 			playSound();
 		}
 	}
 
 	private void playSound()
 	{
-		if (clip != null)
+		if (clip == null)
 		{
-			if (clip.isRunning())
-			{
-				clip.stop();
-			}
-			clip.setFramePosition(0);
-
-			try
-			{
-				FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-				float volume = config.volume();
-
-				float minDb = -20.0f;
-				float maxDb = 6.0f;
-
-				float dB = (volume == 0)
-						? gainControl.getMinimum()
-						: minDb + (volume / 100.0f) * (maxDb - minDb);
-
-				gainControl.setValue(dB);
-			}
-			catch (IllegalArgumentException e)
-			{
-				log.warn("Volume control not supported on this system/clip", e);
-			}
-
-			clip.start();
+			return;
 		}
+
+		if (clip.isRunning())
+		{
+			clip.stop();
+		}
+
+		clip.setFramePosition(0);
+
+		try
+		{
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			float volume = config.volume();
+
+			final float minDb = -20.0f;
+			final float maxDb = 6.0f;
+
+			float dB = (volume == 0)
+					? gainControl.getMinimum()
+					: minDb + (volume / 100.0f) * (maxDb - minDb);
+
+			gainControl.setValue(dB);
+		}
+		catch (IllegalArgumentException e)
+		{
+			log.warn("Volume control not supported", e);
+		}
+
+		clip.start();
 	}
 
-	private void copyDefaultHornToFolder(File destinationFile)
+	private void copyDefaultHornToFile(File destinationFile)
 	{
-		try (var in = getClass().getResourceAsStream("/horn.wav"))
+		try (InputStream in = getClass().getResourceAsStream("/horn.wav"))
 		{
 			if (in == null)
 			{
@@ -144,9 +145,9 @@ public class HornSoundPlugin extends Plugin
 			}
 
 			Files.copy(in, destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			log.info("Default horn.wav copied to {}", destinationFile.getAbsolutePath());
+			log.info("Copied default horn.wav to {}", destinationFile.getAbsolutePath());
 		}
-		catch (Exception e)
+		catch (IOException e)
 		{
 			log.warn("Failed to copy default horn.wav to plugin folder", e);
 		}
